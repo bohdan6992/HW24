c5b1fd3dff9e88321a2f06bec2f36282a87ea913
bohdan6992 <bohdanmaljar@gmail.com>
2021-10-07 00:13:16 +0300

    change 1

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,36 @@
-const arr1 = [];
-const arr2 = [];
-let limit = 5;
+// Задания на урок:
+// Все изминения реализовать только при помощи JS
+// 1) Удалить все рекламные блоки со страницы (правая часть сайта)+
 
-do {
-  let num1 = parseInt(prompt('Enter first number, please'));
-  let num2 = parseInt(prompt('Enter second number, please'));
-
-  let sum = num1 + num2;
-  
-  if (sum %2 == 0){
-    arr1.push(sum);
-  }else{
-    arr2.push(sum);
-  }
-
-} while (arr1.length > 0 && arr1.length < limit || arr2.length > 0 && arr2.length < limit);
-
-if (arr1.length > arr2.length){
-  alert(`First array win!!! Array numbers:[${arr1}]`)
-}else{
-  alert(`Second array win!!! Array numbers:[${arr2}]`)
+const adv = document.querySelector(".promo__adv");
+while (adv.firstChild) {
+  adv.removeChild(adv.firstChild);
 }
+
+// 2) Изменить жанр фильма, поменять "комедия" на "эпическая фантастика"+
+const genre = document.querySelector('.promo__genre');
+genre.innerHTML = 'эпическая фантастика';
+
+// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.+
+const background = document.querySelector('.promo__bg');
+background.style.backgroundImage = "url(img/bg.jpg)";
+
+// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+// Отсортировать их по алфавиту (загуглить специальный метод массивов)
+const movieDB = {
+  movies: [
+    "ГЛАВНЫЙ ГЕРОЙ",
+    "ГЛАДИАТОР",
+    "ГОДЗИЛЛА 2",
+    "ВРЕМЯ",
+    "НИКТО"
+  ]
+};
+
+m = movieDB.movies.sort();
+for (let i = 0; i<5; i++){
+  document.querySelectorAll('.promo__interactive-list li')[i].replaceWith(m[i]);
+}
+
+// 5) Добавить нумерацию выведенных фильмов
+
